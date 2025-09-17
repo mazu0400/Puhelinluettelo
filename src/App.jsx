97ce42da54d55693ad6ab7b718cb24189986355d
mazu0400 <matias.tolvanen13@gmail.com>
2025-09-17 12:55:17 +0300

    Puhelinluettelo step3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-179086" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,12 +19,17 @@ const App = () => {
     }
     const personObject = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -32,15 +40,19 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
-      {/*debug-näyttö*/}
-      <div>debug: {newName}</div>
+
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, i) => (
-          <li key={i}>{person.name}</li>
+          <li key={i}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
     </div>
